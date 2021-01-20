af1affa2c2b57ff888ae0c24bfaa362945433854
Sergio <a2019112566@estudiantes.upsa.edu.bo>
2021-01-20 03:51:16 -0400

    Creación del "People Controller"

---
+++ ApiPersona/WebApiPersona/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApiPersona.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<WebApiPersona.Models.Person> People { get; set; }
+    }
+}
